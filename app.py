a2e690d9397e2ac19fd2046509a800ca773d94e4
Aaron Wagner <aaron.wagner@smawandi.com>
2023-05-03 10:58:01 +0200

    script

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import requests
 import time
 
-def send_request():
+while True:
     response = requests.get('https://dummyjson.com/products/1')
     print(response.json())
-
-if __name__ == '__main__':
-    while True:
-        send_request()
-        time.sleep(5)
+    time.sleep(5)
